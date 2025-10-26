d3e695441bdd34eb5cc9f16ea048dd010212e302
Aneth Morantes <84743013+An-moran@users.noreply.github.com>
2025-10-26 20:31:01 +0000

    Generador de excusas

--- src/app.js
+++ src/app.js
@@ -1,11 +1,37 @@
-import "bootstrap";
-import "./style.css";
+
+let who = [`A pack of dolphins`, `Edward Cullen`, `A granny`, `A racoon`];
+let action = [`made a horrible sound`, `looked in my direction`, `gave me the middle finger`, `robbed me`];
+let what = [`while looking at me`, `with suspicious intentions`, `and that made me sad`, `with ninja-like moves`];
+let when = [`as I was crossing Maracaibo's lake.`, `and gave me a panick attack.`, `as I was crossing the street.`, `and left me in complete shock!`]
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function generarexcusa () {
+
+    let ramnumforwho = Math.floor(Math.random() * 4);
+
+    let ramnumforaction = Math.floor(Math.random() * 4);
+
+    let ramnumforwhat = Math.floor(Math.random() * 4);
+
+    let ramnumforwhen = Math.floor(Math.random() * 4);
+
+    let numforwho = who [ramnumforwho];
+    
+    let numforaction = action [ramnumforaction];
+
+    let numforwhat = what [ramnumforwhat];
+
+    let numforwhen = when [ramnumforwhen];
+
+    let excusa = numforwho + ` ` + numforaction + ` ` + numforwhat + ` ` + numforwhen;
+
+    return excusa;
+}
+
+function cambiarExcusa() {
+    document.getElementById("excusa").innerHTML = generarexcusa();
+}
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+    cambiarExcusa(); 
+}
